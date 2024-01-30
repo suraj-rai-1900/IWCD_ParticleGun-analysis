afb0148657fdaa04a300217c342e449df9a469a7
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-01-30 15:24:30 +0530

    Update Classification_model.py

--- Classification_model.py
+++ Classification_model.py
@@ -54,7 +54,10 @@ def run_model(df, train_col, train_labels, model_name='gbdt', grid_search=False)
     feature_imp = model.get_features_importances()
     print(feature_imp)
 
-    df['Classified_sig'] = (model.y_prob >= model.best_thresh).astype(int)
+    if df['h5_labels'].isin(train_labels).all():
+        df[model_name + '_sig'] = (model.y_prob >= model.best_thresh).astype(int)
+    else:
+        df[model_name + '_sig'] = 0
 
 
 class CutEngine:

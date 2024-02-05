e908fe79e06f1cb57edda1d9d78a094a7005bec9
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-02-05 11:15:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -173,50 +173,6 @@ def plot_sg_bg(df, sig_label, bg_label, key1, key2, x_range, y_range, bin_x, bin
     return fig, axes
 
 
-def return_basic_cuts(df):
-    basic_cuts = (
-            (df['reco_electron_dwall'] > 50)
-            & (df['reco_electron_towall'] > 100)
-            & (df['reco_electron_mom'] > 100)
-            & ~((df['h5_labels'] == 2) & ~(df['h5_momentum'] > 2 * df['h5_dwall']))
-    )
-    return basic_cuts
-
-
-def return_fq_emu_cuts(df):
-    cuts = (
-            (df['e/mu_likelihood ratio'] > 119.8 - 0.32 * df['reco_electron_mom'])
-            & (df['e/mu_likelihood ratio'] > 45.6 - 0.14 * df['reco_electron_dwall'])
-            & (df['e/mu_likelihood ratio'] > 149.1 - 0.69 * df['reco_electron_towall'])
-    )
-    return cuts
-
-
-def return_fq_epi0_cuts(df):
-    cuts = (
-            (df['pi0/e_likelihood ratio'] < 32.2 + 0.42 * df['reco_electron_mom'])
-            & (df['pi0/e_likelihood ratio'] < 165 - 0.1 * df['pi0_mass'])
-            & (df['pi0/e_likelihood ratio'] < 66.2 + 1.92 * df['reco_electron_dwall'])
-            & (df['pi0/e_likelihood ratio'] < 56.5 + 0.95 * df['reco_electron_towall'])
-    )
-    return cuts
-
-
-def return_ml_emu_cuts(df):
-    cuts = (
-        (df['pmu'] < 10 ** (-0.6))
-    )
-    return cuts
-
-
-def return_ml_epi0_cuts(df):
-    cuts = (
-            (df['ppi0'] < 10 ** (-0.6))
-            & (df['pe'] > 10 ** (-1.2))
-    )
-    return cuts
-
-
 def sg_eff(df, cuts, sg_label):
     if not isinstance(sg_label, (list, np.ndarray)):
         sg_label = [sg_label]
